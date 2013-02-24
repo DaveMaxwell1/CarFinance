0d7f38e55c571a9275bddc817633f04671d81a0c
Kevin Sheedy <kevinsheedy@gmail.com>
2013-02-24 14:15:03 +0000

    Added a homepage and an environments page

---
+++ app/controllers/EnvironmentsController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class EnvironmentsController extends Controller {
+  
+	public static Result list() {
+		return ok(environmentsView.render("Your new application is ready."));
+	}
+
+}
